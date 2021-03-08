2ac0f0e232b08bbae8cad27e6ec1707d09ed6312
Miranda Wilson <mirandascwilson@gmail.com>
2021-03-08 14:10:09 +0000

    Allow printing of bank statement

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,10 +4,23 @@ class BankAccount
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(amount, date)
     @balance += amount
+    @transactions << { amount: amount, date: date }
+  end
+
+  def withdraw(amount, date)
+    @balance -= amount
+  end
+
+  def print_statement
+    header = "date || credit || debit || balance"
+    transaction = "#{@transactions[-1][:date].strftime('%d/%m/%Y')} || 1000.00 || || 1000.00"
+
+    print header + "\n" + transaction
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,10 +1,30 @@
 require "bank_account"
 
-describe "bank_account" do
+describe BankAccount do
+
+  let(:account) { BankAccount.new }
+  let(:statement_header) { "date || credit || debit || balance\n" }
 
   it "allows deposits to change balance" do
-    account = BankAccount.new
     expect{ account.deposit(100, Time.now) }.to change{ account.balance }.by 100
+  end
+
+  it "allows withdrawals to change balance" do
+    account.deposit(100, Time.now)
+    expect{ account.withdraw(100, Time.now) }.to change{ account.balance }.by -100
+  end
+
+  it "prints out a statement from given data" do
+    transaction = "10/01/2012 || 1000.00 || || 1000.00"
+    account.deposit(1000, Time.new(2012, 1, 10))
+    expect{ account.print_statement }.to output(statement_header + transaction).to_stdout
+  end
+
+  it "prints out a statement based on current date" do
+    date = Time.now
+    transaction = "#{date.strftime('%d/%m/%Y')} || 1000.00 || || 1000.00"
+    account.deposit(1000, Time.now)
+    expect{ account.print_statement }.to output(statement_header + transaction).to_stdout
   end
 
 end
